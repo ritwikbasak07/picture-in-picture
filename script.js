cdbb59104f0d0308897e27331cb23fe8c1f68db5
Ritwik Basak <88833195+ritwikbasak07@users.noreply.github.com>
2022-10-08 16:07:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// prompt to select a media stream , pass to video element , then play
+
+async function selectMediaSATream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+button.addEventListener('click', async () => {
+    // Disable Button
+    button.disabled = true;
+    // Start Picture in Picture
+    await videoElement.requestPictureInPicture();
+    // Reset Button
+    button.disabled = false;
+});
+
+// On load
+selectMediaSATream();
